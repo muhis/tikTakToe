97eb1c5478972a8fd4f9af962255852af55d3637
= <m.salman@zinkki.com>
2017-01-21 09:59:40 +0200

    1. Adding drawing function. 2. Adding text parser with empty element detection. 3. Finalizing the front page with input elements. 4. Adding some explination for the drawing function.

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -16,15 +16,16 @@ tTTAnalyze = function(moves){
     //First point in this diagonal is zero, second point is (d + 1) because we are starting from zero index instead of (1).
     secondPoint = d + 1;
     for(i =secondPoint; i <= bottomRightCorner;i += (d+1)){
-      if(moves[i] == moves[0]){
-        if(i == bottomRightCorner){
-          return{
-            size: d,
-            winner: moves[0],
-            game: true,
-            end: 'win',
-            winType: 'dia-LTR',
-            point: i
+      if(moves[i]){
+        if(moves[i] == moves[0]){
+          if(i == bottomRightCorner){
+            return{
+              size: d,
+              winner: moves[0],
+              end: 'win',
+              winType: 'dia-LTR',
+              point: i
+            }
           }
         }
       }else{break}
@@ -32,15 +33,16 @@ tTTAnalyze = function(moves){
 
     //Second diagonal is from the top right corner to bottom left corner, the step is (d-1) which is also the top right corner
     for(i = topRightCorner; i <= bottomLeftCorner;i += topRightCorner){
-      if(moves[i] == moves[topRightCorner]){
-        if(i == bottomLeftCorner){
-          return{
-            size: d,
-            winner: moves[i],
-            game: true,
-            end: 'win',
-            winType: 'dia-RTL',
-            point: i
+      if(moves[i]){
+        if(moves[i] == moves[topRightCorner]){
+          if(i == bottomLeftCorner){
+            return{
+              size: d,
+              winner: moves[i],
+              end: 'win',
+              winType: 'dia-RTL',
+              point: i
+            }
           }
         }
       }else{break}
@@ -53,12 +55,12 @@ tTTAnalyze = function(moves){
       secondCell = topCell + d;
       //start from second cell because the comparison is done with the first cell.
       for(j = secondCell; j <= bottomCell; j += d){
+        if(moves[j]){
           if(moves[j]==moves[topCell]){
             if(j == bottomCell){
           return{
             size: d,
             winner: moves[j],
-            game: true,
             end: 'win',
             winType: 'col',
             point: topCell
@@ -67,22 +69,24 @@ tTTAnalyze = function(moves){
           }else{
             break;
           }
+        }
       }
     }
     //Check the rows
     for(i = 0; i <= bottomLeftCorner;i += d){
       //will start from the second item in the row since the comaprison is done with the first item(j = i+1) instead of (j = i)
       for(j =i+1; j <= (i+d-1);j++){
-        if(moves[j]==moves[i]){
-          if(j == (i+d-1)){
-          return{
-            size: d,
-            winner: moves[j],
-            game: true,
-            end: 'win',
-            winType: 'row',
-            point: i
-          }
+        if(moves[j]){
+          if(moves[j]==moves[i]){
+            if(j == (i+d-1)){
+              return{
+                size: d,
+                winner: moves[j],
+                end: 'win',
+                winType: 'row',
+                point: i
+              }
+            }
           }
         }else{
           break;
@@ -92,29 +96,38 @@ tTTAnalyze = function(moves){
     //If there is no win by this step, it means there is a draw
     return{
       size: d,
-      game: true,
       end: 'draw'
     }
 
   }else{
     //Borad is not square!
   return{
-    game: false
+    end: 'notAGame'
   }
   }
 
 }
+/* draw
+This function deals with tTTAnalyze function output to draw the board and the winner for a better illustration of the game.
+The function takes a div that acts like a canvas for drawing, and moves array as input.
+Author: Mohammed Salman
+github.com/muhis
+*/
 draw = function(divID, array){
   result = tTTAnalyze(array);
   d = result.size;
   document.getElementById(divID).innerHTML = '';
+  document.getElementById("result").innerHTML = '';
+  //draw the board, using the size provided in (result.size) to make a square with smaller squares that contain cell value (player name).
   for(i=0;i <= (d-1)*d; i += d){
     for(j=i; j <= (i+d-1); j++){
-      document.getElementById(divID).innerHTML += '<div class="cell" id="' + j + '">' + array[j] + '</div>';
+      if(array[j]==false){elm = "-"}else{elm= array[j]}
+      document.getElementById(divID).innerHTML += '<div class="cell" id="' + j + '">' + elm + '</div>';
     }
     document.getElementById(divID).innerHTML += '</br>'
   }
   if(result.end == 'win'){
+    document.getElementById("result").innerHTML = '<h4>The winner is: ' + result.winner + '</h4>';
     if(result.winType == 'dia-LTR'){
       for(i=0; i <= ((d*d)-1); i += d+1){
         toggleWin(i);
@@ -132,12 +145,17 @@ draw = function(divID, array){
       }
     }
     if(result.winType == 'row'){
-      for(i=result.point; i <= result.point + d; i++){
+      for(i=result.point; i <= result.point + d -1; i++){
         toggleWin(i);
       }
     }
   }else{
-    //draw
+    if(result.end == 'notAGame'){
+      document.getElementById("result").innerHTML = '<h4>Not a valid game</h4>';
+    }
+    if(result.end == 'draw'){
+      document.getElementById("result").innerHTML = '<h4>The game was strong draw!<h4>';
+    }
   }
 }
 function toggleWin(div){
@@ -145,7 +163,15 @@ function toggleWin(div){
 
 }
 function parseTextInside(elmnt){
-  array = document.getElementByID(elmnt).innerHTML;
-  arrayElement = JSON.parse(array);
-  draw('canvas',arrayElement);
+  array = document.getElementById(elmnt).value;
+  if(array){
+    arrayElement = JSON.parse(array);
+    for(i=0;i <= array.length;i++){
+      if(array[i]==""){
+        array[i]=false;
+      }
+    }
+    draw('canvas',arrayElement);
+  }
+
 }
